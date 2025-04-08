bbb16dd082646fc92a09eec9d2daf7307ae969b8
kartikkalyanshetti <kalyanshettikartik11@gmail.com>
2025-04-08 22:06:19 +0530

    Added Basics folder as normal content

---
+++ Basics/src/Basics/Basics.java
@@ -0,0 +1,14 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class Basics {
+
+    public static void main(String[] args) {
+        // input 
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        // output 
+        System.out.println(a);
+    }
+}
