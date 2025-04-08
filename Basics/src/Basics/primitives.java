78390237afc842bce265c59f65985eb34b862251
kartikkalyanshetti <kalyanshettikartik11@gmail.com>
2025-04-08 22:43:46 +0530

    added primitives

---
+++ Basics/src/Basics/primitives.java
@@ -0,0 +1,14 @@
+package Basics;
+
+public class primitives {
+    String name = "Kartik";
+
+    //Primitive data types in Java : Means Breaking the data into further pieces. 
+   
+    int rollno = 11;
+    char letter = 'r';
+    float marks = 99.99f; // if i wanna use float write f at the end .
+    double largeDecimals = 474673724.2482;
+    long largeInteger = 23523567123562173L; // to store larger values and at the end L is used
+    boolean check = false;
+}
