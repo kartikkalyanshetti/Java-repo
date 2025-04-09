9f4e5dba1acf80e85e78a767a060281cb1f473d2
kartikkalyanshetti <kalyanshettikartik11@gmail.com>
2025-04-09 23:26:15 +0530

    if - else code added

---
+++ Conditions and loops/src/condition.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class condition {
+    public static void main(String[] args) {
+                Scanner input = new Scanner(System.in);
+        Long Salary = input.nextLong();
+
+        if(Salary < 10000){
+            System.out.println("You are poor");
+    }
+        else if (Salary >= 10000 && Salary < 20000){
+            System.out.println("You are middle class");
+    }
+    else{
+        System.out.println("You are rich");
+    }
+    }
+}
